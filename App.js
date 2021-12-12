d4f5670078613e4b219b6ff17552f462d1303fed
Michaela <89934969+MichaelaLoretta@users.noreply.github.com>
2021-12-12 21:32:33 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator,
+  View,
+} from "react-native";
 
-export default function App() {
+const MovieApp = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://reactnative.dev/movies.json")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json.movies);
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
+      })
+      .catch((e) => console.log(e));
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isLoading ? (
+        <ActivityIndicator size={"small"} color={"black"} />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }) => id}
+          renderItem={({ item }) => (
+            <View style={styles.listBox}>
+              <View style={styles.listStyle}>
+                <Text>{item.id} </Text>
+              </View>
+              <View style={styles.listStyle}>
+                <Text>{item.title}</Text>
+              </View>
+              <View style={styles.listStyle}>
+                <Text> {item.releaseYear}</Text>
+              </View>
+            </View>
+          )}
+        />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 80,
+    backgroundColor: "#B392AC",
+    alignItems: "center",
+  },
+  listBox: {
+    flexDirection: "row",
+    backgroundColor: "#D1B3C4",
+    padding: 20,
+  },
+  listStyle: {
+    justifyContent: "center",
+    height: 40,
+    width: 100,
+    borderBottomColor: "#000",
+    borderBottomWidth: 1,
   },
 });
+
+export default MovieApp;
